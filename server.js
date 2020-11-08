d8d45a33644b5cce228ca5f794a82c6980835ac2
Vale314 <electrovale314@hotmail.com>
2020-11-08 14:26:00 -0600

    local

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const server = require("http").createServer(app);
 // Init Middleware
 app.use(express.json({ extended: false, limit: "5mb" }));
 
+// Define Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/register", require("./routes/register"));
+
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
